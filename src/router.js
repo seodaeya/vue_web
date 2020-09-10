3bb7fc5ec6c2689d4165c24c79f5461ec5353283
key <dsseo.uzen@amorepacific.com>
2020-09-10 12:24:03 +0900

    라우터 써서 메인 화면 띄우기

---
+++ src/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Main from './views/Main.vue'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    mode: 'history', // 
+    base: process.env.BASE_URL, // 따로 지정하지 않는다면 default '/'를 갖는다.
+    routes: [
+        {
+            path: '/',
+            name: 'main',
+            component: Main
+        }/* ,
+      {
+        path: '/page2',
+        name: 'page2',
+        component: () => import('./views/Page2.vue')
+      } */
+    ]
+})
---
+++ src/views/Main.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="main">
+        내가 메인이다!
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
