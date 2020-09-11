3a703aaa351d59e090a971eda95ee3c1a301507a
key <dsseo.uzen@amorepacific.com>
2020-09-11 16:16:31 +0900

    셀렉트박스 변경해서 이미지 변경하기

--- src/views/Api.vue
+++ src/views/Api.vue
@@ -5,17 +5,19 @@
     <h1>#목록 테스트</h1>
     <template v-for="map in testList">
       <div v-bind:key="map.main_key">
-        <img src="https://picsum.photos/id/3/100/100" class="image" />
+        <img src="https://picsum.photos/id/3/100/100" />
         {{map.main_key}} / {{map.sub_key}}
       </div>
     </template>
 
-    <h1>#셀렉트박스 테스트</h1>
+    <h1>#셀렉트박스 테스트 + 변경 시, 이미지 변경</h1>
     <template>
-        <select v-model="testSelected">
+        <select v-model="testSelected" @change="f_selectbox_change" >
             <option v-for="map in testList" v-bind:key="map.main_key" v-bind:value="map.main_key">{{ map.sub_key }}</option>
         </select>
     </template>
+
+    <img v-bind:src="imgSrc" />
   </div>
 </template>
 
@@ -25,8 +27,9 @@ import axios from "axios";
 export default {
   data() {
     return {
-      testList: []
-      ,testSelected:'value2_1'
+      testList      : []
+      ,testSelected : 'value2_1'
+      ,imgSrc       : 'https://picsum.photos/id/4/100/100'
     };
   },
   mounted() {
@@ -46,6 +49,10 @@ export default {
     f_btn_click() {
       console.log("버튼 눌렀쥬~");
     },
+    f_selectbox_change(value) {
+      console.log("셀렉트박스가 바뀌었쥬~")
+      this.imgSrc = 'https://picsum.photos/id/' + ('value1_1' === value.target.value ? 1 : 2) + '/100/100'
+    }
   },
 };
 </script>
