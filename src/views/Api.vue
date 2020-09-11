0cd065ad5f01c296dff7f6dc9169462e3c8b1edb
key <dsseo.uzen@amorepacific.com>
2020-09-11 15:45:30 +0900

    셀렉트박스 추가

--- src/views/Api.vue
+++ src/views/Api.vue
@@ -2,11 +2,19 @@
   <div class="api">
     <button @click="f_btn_click">버튼 테스트</button>
 
+    <h1>#목록 테스트</h1>
     <template v-for="map in testList">
       <div v-bind:key="map.main_key">
         <img src="https://picsum.photos/id/3/100/100" class="image" />
         {{map.main_key}} / {{map.sub_key}}
       </div>
+    </template>
+
+    <h1>#셀렉트박스 테스트</h1>
+    <template>
+        <select v-model="testSelected">
+            <option v-for="map in testList" v-bind:key="map.main_key" v-bind:value="map.main_key">{{ map.sub_key }}</option>
+        </select>
     </template>
   </div>
 </template>
@@ -18,6 +26,7 @@ export default {
   data() {
     return {
       testList: []
+      ,testSelected:'value2_1'
     };
   },
   mounted() {
